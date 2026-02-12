1052523cdec70c88dcbfb107bb57f0fe0139e1c6
Aalekh-coder <dheetaalekh@gmail.com>
2026-02-12 10:29:23 +0530

    25 deployed

--- app/sales-dashboard/Layout.jsx
+++ app/sales-dashboard/Layout.jsx
@@ -1,32 +1,11 @@
 "use client";
 
-import SalesNavbar from "@/components/layout/sales-dashboard/SalesNavbar";
-import SalesSidebar from "@/components/layout/sales-dashboard/SalesSidebar";
-import { useSalesEmployeeStore } from "@/lib/store/salesEmployeeStore";
+import Layout from "@/components/layout/sales-dashboard/Layout";
+
 import React, { useEffect, useState } from "react";
 
 export default function SalesLayout({ children }) {
-  const [open, setOpen] = useState(false);
-  const fetchEmployee = useSalesEmployeeStore((s) => s.fetchEmployee);
-  const { employee, loading } = useSalesEmployeeStore();
 
-  useEffect(() => {
-    if (!employee) {
-      fetchEmployee().then(() => {
-        console.log("Employee fetched");
-      });
-    }
-  }, []);
 
-  if (loading) return null; 
-
-  return (
-    <div className="flex min-h-screen bg-gray-100">
-      <SalesSidebar open={open} setOpen={setOpen} />
-      <div className="flex-1 flex flex-col h-screen overflow-hidden">
-        <SalesNavbar />
-        <main className="flex-1 overflow-y-auto p-6">{children}</main>
-      </div>
-    </div>
-  );
+  return <Layout>{children}</Layout>;
 }
